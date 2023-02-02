df8cfa61b0d11441240a89f5be7fcc5caae7bdb3
sujith-sairam <89727004+sujith-sairam@users.noreply.github.com>
2023-02-02 14:57:53 +0530

    completed checkout page

--- src/components/routes/checkout/checkout.js
+++ src/components/routes/checkout/checkout.js
@@ -1,27 +1,38 @@
 import { useContext } from 'react';
 import { CartContext } from '../../../contexts/cart-item.context';
+import CheckoutItem from '../../checkout-item/checkout-item';
 import './checkout.styles.scss';
 
 function Checkout(){
-    const { cartItems,addItemToCart, removeItemToCart } = useContext(CartContext);
+    const { cartItems,cartTotal } = useContext(CartContext);
     return(
-      <div>
-        <h1>CheckOut Page</h1>
-        <div>
-            { cartItems.map((cartItem) => {
-                const {id, name, quantity} = cartItem;
-                return (
-                    <div key={id}>
-                        <h2>{name}</h2>
-                        <span>{quantity}</span>
-                        <br/>
-                        <span onClick={() => removeItemToCart(cartItem) } >decrement</span>
-                        <br/>
-                        <span onClick={() => addItemToCart(cartItem)}>increment</span>
-                        </div>
-                )
-                })}
+      <div className='checkout-container'>
+        <div className='checkout-header'>
+           <div className='header-block'>
+                <span>Product</span>
+           </div>
+
+           <div className='header-block'>
+                <span>Description</span>
+           </div>
+
+           <div className='header-block'>
+                <span>Quantity</span>
+          </div>
+
+          <div className='header-block'>
+                <span>Price</span>
+          </div>
+                
+          <div className='header-block'>
+                <span>Remove</span>
+          </div>
         </div>
+        
+        { cartItems.map((cartItem) => (
+             <CheckoutItem key={cartItem.id} cartItem = {cartItem} />
+          ))}
+      <span className='total'>Total : ${cartTotal}</span>
       </div>
     );
 }
--- src/contexts/cart-item.context.jsx
+++ src/contexts/cart-item.context.jsx
@@ -34,13 +34,19 @@ const removeCartItem = (cartItems , cartItemToRemove) => {
  );
 }
 
+const clearCartItem = (cartItems,cartItemToClear) => {
+    return cartItems.filter((cartItem) =>  cartItem.id !== cartItemToClear.id );
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
     setIsCartOpen: () => {},
     cartItems: [],
     addItemToCart: () => {},
     removeItemFromCart: () => {},
-    cartCount : 0
+    clearItemFromCart: () => {},
+    cartCount : 0,
+    cartTotal: 0
 });
 
 
@@ -48,11 +54,17 @@ export const CartProvider = ({children}) => {
     const [isCartOpen , setIsCartOpen] = useState(false);
     const [cartItems, setCartItems] = useState([]);
     const [cartCount, setCartCount] = useState(0);
+    const [cartTotal, setCartTotal] = useState(0);
     
 useEffect(() => {
    const newCartCount = cartItems.reduce((total, cartItem) => total + cartItem.quantity , 0);
    setCartCount(newCartCount);
 },[cartItems]);
+
+useEffect(() => {
+    const newCartTotal = cartItems.reduce((total, cartItem) => total + cartItem.quantity * cartItem.price , 0);
+    setCartTotal(newCartTotal);
+ },[cartItems]);
 
 const addItemToCart = (product) => {
     setCartItems(addCartItem(cartItems,product));
@@ -62,7 +74,18 @@ const removeItemToCart = (cartItemToRemove) => {
     setCartItems(removeCartItem(cartItems,cartItemToRemove));
 }
 
-const value = { isCartOpen, setIsCartOpen, cartItems, addItemToCart , cartCount, removeItemToCart} ;
+const clearItemFromCart= (cartItemToClear) => {
+    setCartItems(clearCartItem(cartItems, cartItemToClear));
+}
+
+const value = { isCartOpen,
+                setIsCartOpen,
+                cartItems,
+                addItemToCart ,
+                cartCount,
+                removeItemToCart,
+                clearItemFromCart,
+                cartTotal } ;
 
     return <CartContext.Provider value={value}>{children}</CartContext.Provider>
 };
