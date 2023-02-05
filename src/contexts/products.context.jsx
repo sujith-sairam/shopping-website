df3a21ac829e91a798b23d254c04533629b19eba
sujith-sairam <89727004+sujith-sairam@users.noreply.github.com>
2023-02-05 11:06:10 +0530

    added data to firebase

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,12 +1,13 @@
+import { useEffect } from "react";
 import { createContext, useState } from "react";
-import PRODUCTS from '../shop-data.json';
 
 export const ProductsContext = createContext({
     products: [],
 });
 
 export const ProductsProvider = ({children}) => {
-    const [products,setProducts] = useState(PRODUCTS);
+    const [products,setProducts] = useState([]);
+   
     const  value  = { products };
     return(
         <ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -15,7 +15,9 @@ import {
     getFirestore,
     doc,
     getDoc,
-    setDoc
+    setDoc,
+    collection,
+    writeBatch,
 } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -42,6 +44,19 @@ const firebaseConfig = {
   export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleprovider);
    
   export const db = getFirestore();
+
+  export const addCollectionAndDocuments = async (collectionKey,objectsToAdd) =>{
+       const collectionRef = collection(db,collectionKey);
+       const batch = writeBatch(db);
+
+       objectsToAdd.forEach((object) => {
+        const docRef = doc(collectionRef,object.title.toLowerCase());
+        batch.set(docRef,object);
+       });
+
+       await batch.commit();
+       console.log("Done");
+  };
 
   export const createUserDocumentFromAuth = async(userAuth,additionalInformation={}) => {
        if(!userAuth) return;
